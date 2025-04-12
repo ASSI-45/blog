2b0f7d578573feeebb03f597d33f8f2a408d5d8b
ASSI-45 <aslan.umarovofficieel@gmail.com>
2025-04-12 11:34:30 +0200

    version 0.0.5

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+export default function Header() {
+  return(
+    <>
+      <div className="flex justify-between p-3 px-8 bg-zinc-900 text-white font-inter">
+        <h1 className="">ASSI-45</h1>
+        <div>
+          <a href="" className="px-8">home</a>
+          <a href="">about</a>
+        </div>
+      </div>
+    </>
+  );
+}
