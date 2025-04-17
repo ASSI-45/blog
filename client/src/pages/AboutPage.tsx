5cc8db73c98832dde709794e79b2f534b01951ee
ASSI-45 <aslan.umarovofficieel@gmail.com>
2025-04-17 14:43:06 +0200

    moved react to client folder

---
+++ client/src/pages/AboutPage.tsx
@@ -0,0 +1,27 @@
+
+export default function AboutPage() {
+  return (
+    <>
+        <h1 className="text-4xl">Hi!</h1>
+        <br />
+        <p>
+          I am Aslan Umarov and an upcoming frontend dev, looking for any work experince as a developer. I wan't to end
+          my work life as a DevOps engineer, So next up is full stack development. And have been programming sinds
+          the age of 14. At first it was something not that serious and for fun. But now i am putting the pedal to the floor. To see how much succes i can realy achive.
+            
+        </p>
+        <br />
+        <p>
+          Right now i am learning React, Node.js, Express and Mongo DB. Thats right the MERN stack.
+          Currenlty also learning pen testing for security reasons . So i can develop beter websites.  
+        </p>
+        <br />
+        <p>Your currenlty reading this from my Raspberry Pi 5!
+
+
+
+      
+        </p>
+    </>
+  )
+}
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+  return(
+    <>
+      <h1 className="text-4xl">Hello world! This site is still under construction 🏗️</h1>
+    </>
+  );
+}
+
