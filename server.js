df1e3d936b89003ead6d380ac1d2dc1f0800f3a2
ASSI-45 <aslan.umarovofficieel@gmail.com>
2025-04-26 23:15:36 +0200

    fixed node error

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname,"../client/dist")))
+app.use(express.static(path.join(__dirname,"./client/dist")))
 
 app.get("/*name", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"), err)
 });
 
 app.listen(3000, () => {
